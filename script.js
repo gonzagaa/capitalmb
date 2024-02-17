b4f600a7fecfa0514b8d4a17a19555aca1dba562
“gonzagaa” <iigust4vo@gmail.com>
2024-02-16 22:11:11 -0300

    update

--- script.js
+++ script.js
@@ -34,6 +34,6 @@ ScrollReveal().reveal('#home h1, #sobre h1, #regulamentos h1', { distance: '400p
 
 ScrollReveal().reveal('h2, #topics .card, #topics ion-icon, #tableprofit button, #beneficios .card, #sobre p, #regulamento h3', { distance: '400px', origin: 'bottom', opacity: 0, duration: 600, easing: 'ease-in-out'});
 
-ScrollReveal().reveal('.buttons, #home .text p, .details details.left, #sobre .button, #regulamentos .button, #regulamentos ul, #regulamentos p', { distance: '100px', origin: 'left', opacity: 0, duration: 600, easing: 'ease-in-out'});
+ScrollReveal().reveal('.buttons, #home .text p, .details details.left, #sobre .button, #regulamentos .button, #regulamentos ul, #regulamentos p', { distance: '100px', origin: 'bottom', opacity: 0, duration: 600, easing: 'ease-in-out'});
 
-ScrollReveal().reveal('#home .image, #tableprofit .table, .details details.right', { distance: '100px', origin: 'right', opacity: 0, duration: 600, easing: 'ease-in-out'});
+ScrollReveal().reveal('#home .image, #tableprofit .table, .details details.right', { distance: '100px', origin: 'bottom', opacity: 0, duration: 600, easing: 'ease-in-out'});
